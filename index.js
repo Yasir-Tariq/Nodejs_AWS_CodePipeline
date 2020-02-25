64866badeb2ebc9a57da8fef630aea7d77f6fd36
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-25 14:24:27 -0800

    final version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('THIS IS SAMPLE NODEJS APPLICATION!\n');
+  res.end('THIS IS FINAL TESTING FOR NODE JS APPLICATION!\n');
 });
 
 server.listen(port, () => {
