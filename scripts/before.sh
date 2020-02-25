0d3653a265f7f628c89ef05254de3bd9af4531ed
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-25 10:58:39 -0800

    new

--- scripts/before.sh
+++ scripts/before.sh
@@ -2,4 +2,4 @@
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 apt-get install -y nodejs
 # rm -f package-lock.json
-if [ -fe package-lock.json ]; then rm -f package-lock.json; fi
+if [ -f package-lock.json ]; then rm -f package-lock.json; fi
