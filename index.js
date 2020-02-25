9e14c8bd2d8efb6a3554a89b001c80faaf7441e9
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-25 13:39:27 -0800

    final version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello EURUS TECHNOLOGIES!\n');
+  res.end('WELCOME TO EURUS TECHNOLOGIES!\n');
 });
 
 server.listen(port, () => {
