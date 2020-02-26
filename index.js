58b0ae20e3a8f7195f0595dca3f2eda4e2d97082
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-26 10:35:44 -0800

    with aws code build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('THIS IS A SAMPLE NODE JS APPLICATION!\n');
+  res.end('THIS IS A SAMPLE NODE JS APPLICATION with code build!\n');
 });
 
 server.listen(port, () => {
